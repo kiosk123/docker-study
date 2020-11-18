fe897104d2c3287b7977d199c38aef9a2f805d81
Heo Jong Tae <heo9910@naver.com>
2020-11-19 03:04:16 +0900

    .

---
+++ 04-1.컨테이너로깅_json-file로그.sh
@@ -0,0 +1,27 @@
+# 컨테이너 생성
+sudo docker run -d --name mysql \
+-e MYSQL_ROOT_PASSWORD=1234 \
+mysql:5.7
+
+# 컨테이너 내부에서  발생하는 출력확인 
+sudo docker logs mysql
+
+# 로그의 마지막 두줄만 출력
+sudo docker logs --tail 2 mysql
+
+# 유닉스 시간으로 특정 시간이후의 로그를 출력
+sudo docker logs --since 1474765979 mysql
+
+# 컨테이너에서 실시간으로 출력되는 내용을 확인 (-t옵션은 타임스탬프 출력)
+sudo docker logs -f -t mysql
+
+# 기본적으로 컨테이너 로그는 JSON형태로 도커내부에 저장
+# 파일은 다음과 같은 경로에 컨테이너 ID로 시작하는 파일명으로 저장됨
+# /var/lib/docker/containers/${CONTAINER_ID}/${CONTAINER_ID}-json.log
+
+# 컨테이너 실행시 json 로그 파일의 최대 크기 지정 max-size
+# 컨테이너 실행시 로그 파일의 개수 지정 max-file
+sudo docker run -it \
+--log-opt max-size=10k --log-opt max-file=3 \
+--name log-test ubuntu:14.04
+
---
+++ 04-2.컨테이너로깅_syslog로그.sh
@@ -0,0 +1,64 @@
+# syslog로 컨테이너를 로깅하도록 설정 - 컨테이너는 syslogtest를 출력 후 바로 종료
+sudo docker run -d --name syslog_container \
+--log-driver=syslog \
+ubuntu:14.04 \
+echo syslogtest
+
+# syslog에 로깅 되었는지 확인 - centos7에는 /var/log/messages를 확인
+tail /var/log/syslog
+
+# syslog를 원격에 저장하는 방법의 하나인 rsyslog를 써서 중앙 컨테이너로 로그를 저장할 수 있다.
+# rsyslog 서비스가 시작되도록 설정된 컨테이너를 구동하고, 클라이언트 호스트에서 컨테이너를 생성해
+# 서버의 rsyslog 컨테이너에 로그를 저장한다.- 
+
+# rsyslog 컨테이너를 생성
+sudo docker run -i -t \
+-h rsyslog \
+--name rsyslog_server \
+-p 514:514 -p 514:514/udp \
+ubuntu:14.04
+
+# 컨테이너 내부의 rsyslog.conf 파일 열어서 syslog 서버 구동시키는 항목의 주석 제거
+vi /etc/rsyslog.conf
+
+# provides UDP syslog reception
+$ModLoad imudp
+$UDPServerRun 514
+
+# provides TCP syslog reception
+$ModLoad imtcp
+$InputTCPServerRun 514
+
+# 컨테이너의 rsyslog 서비스 재시작
+service rsyslog restart
+
+# 클라이언트 호스트에서 아래의 명령어를 입력해 컨테이너를 생성한다.
+# 그리고 컨테이너 로그를 기록하기 위한 간단한 echo 명령을 실행한다.
+# 여기서는 tcp로 방법으로 활성화 했지만 rsyslog.conf에 udp설정을 해서
+# udp로도 사용할 수도 있다.
+sudo docker run -i -t \
+--log-driver=syslog \
+--log-opt syslog-address=tcp://192.168.0.164:514 \
+--log-opt tag="mylog" \
+ubuntu:14.04
+
+echo test
+
+# 다시 rsyslog 컨테이너에 접속해 /var/log/syslog에 로그가 기록되었는지 확인한다.
+sudo docker attach rsyslog_server
+
+tail /var/log/syslog
+
+# --log-opt로 syslog-facility를 쓰면 로그가 저장될 파일을 바꿀수 있다.
+# 로그를 생성하는 주체(클라이언트)에 따라 로그를 다르게 저장하는 것으로
+# 여러 애플리케이션에서 수집되는 로그를 분류할 수 있다.
+# 기본적으로 daemon으로 설정 되어있지만 kern, user, mail 등 다른 facility도 사용가능하다
+sudo docker run -i -t \
+--log-driver=syslog \
+--log-opt syslog-address=tcp://192.168.0.164:514 \
+--log-opt tag="maillog" \
+--log-opt syslog-facility="mail" \
+ubuntu:14.04
+
+# rsyslog 서버 컨테이너의 /var/log 디렉터리에 facility에 해당하는 로그파일이 
+# 생성된다. 위에서는 mail을 사용했으므로 mail.log라는 파일이 생성된다.
