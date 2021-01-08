f82ee52490895567bf85901d501e5fb50e5350aa
Heo Jong Tae <heo9910@naver.com>
2021-01-08 22:11:11 +0900

    .

--- 08.도커데몬.sh
+++ 08.도커데몬.sh
@@ -1,14 +1,17 @@
-# 도커 데몬 실행 및 종료 - service 명령어 사용
+#---------------------------------------------------------------------------------------------------
+# 도커 데몬 실행
+
+# 1. 도커 데몬 실행 및 종료 - service 명령어 사용
 sudo service docker start
 sudo service docker stop
 
-# 도커 데몬 실행, 활성화(재부팅되도 실행), 종료 - systemctl 명령어 사용
+# 2. 도커 데몬 실행, 활성화(재부팅되도 실행), 종료 - systemctl 명령어 사용
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl stop docker
 
-# 서비스로 실행이 아닌 명령어로 도커 데몬 실행 --insecure-registry 옵션 추가.
-# 일반적으로 /etc/default/docker의 DOCKER_OPTS에 옵션을 설정 후 사용 - 06.도커이미지_도커허브_사설레지스트리.sh 참고 
+# 3. 서비스로 실행이 아닌 명령어로 도커 데몬 실행 --insecure-registry 옵션 추가.
+#    일반적으로 /etc/default/docker의 DOCKER_OPTS에 옵션을 설정 후 사용 - 06.도커이미지_도커허브_사설레지스트리.sh 참고 
 sudo dockerd -H tcp://0.0.0.0:2375 --insecure-registry=192.168.0.164:5000 --tls=false
 
 # DOCKER_OPTS에 설정할 경우
@@ -34,4 +37,12 @@ sudo dockerd -H unix:///var/run/docker/sock -H tcp://0.0.0.0:2375
 sudo docker -H tcp://192.168.0.164:2375 
 sudo docker -H tcp://192.168.0.164:2375 images # 원격으로 도커데몬의 이미지 목록 확인
 curl 192.168.0.164:2375/version --silent | python -m json.tool # curl을 이용하여 도커 버전 확인
+
+# 4-1. 환경 변수를 설정하면 원격지 주소를 생략하고 명령어 실행 가능
+export DOCKER_HOST="tcp://tcp://192.168.0.164:2375"
+sudo docker version
+
+#---------------------------------------------------------------------------------------------------
+# 도커 데몬 보안 적용
+
 
